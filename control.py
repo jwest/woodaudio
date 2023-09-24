9a623d03d3c3d2fd1a11fd2fe25d20ee6fd235ad
Jakub Westfalewski <j.westfalewski@gmail.com>
2023-09-24 10:38:12 +0200

    event based control

--- control.py
+++ control.py
@@ -6,24 +6,21 @@ import time
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    input_state1 = GPIO.input(4)
-    input_state2 = GPIO.input(23)
-    input_state3 = GPIO.input(24)
-    if input_state1 == False:
-        print('Button 1 Pressed')
-        r.publish('player:control', 'PLAY_OR_PAUSE')
-        time.sleep(0.2)
-    if input_state2 == False:
-        print('Button 2 Pressed')
-        r.publish('player:control', 'LIKE')
-        time.sleep(0.2)
-    if input_state3 == False:
-        print('Button 3 Pressed')
-        r.publish('player:control', 'PLAY_OR_NEXT')
-        time.sleep(0.2)
+def like(_):
+  print('"LIKE" command send to redis')
+  r.publish('player:control', 'LIKE')
+
+def play_or_next(_):
+  print('"PLAY_OR_NEXT" command send to redis')
+  r.publish('player:control', 'PLAY_OR_NEXT')
+
+GPIO.add_event_detect(23, GPIO.FALLING, callback=like, bouncetime=300)
+GPIO.add_event_detect(24, GPIO.FALLING, callback=play_or_next, bouncetime=300)
+
+try:
+    while True : time.sleep(0.2)
+except:
+    GPIO.cleanup()
